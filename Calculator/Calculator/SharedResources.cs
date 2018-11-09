d68f973b74ffee4d21bf9f88ccbba13ba5772b1a
Alessandro <a.bellotti@patente.it>
2018-11-09 11:44:48 +0100

    Aggiungere i file di progetto.

---
+++ Calculator/Calculator/SharedResources.cs
@@ -0,0 +1,28 @@
+﻿
+using Xamarin.Forms;
+
+namespace Calculator
+{
+    public static class SharedResources
+    {
+        public static Color MyOpButtonBkColor
+        {
+            get { return Color.FromRgb(0xff, 0xa5, 0xa5); }
+        }
+
+        public static Color OpButtonBkColor
+        {
+            get { return Color.FromRgb(0xff, 0xa5, 0); }
+        }
+
+        public static double CustomFontSize
+        {
+            get { return 34; }
+        }
+
+        public static Color MyDarkGrayTextColor
+        {
+            get { return Color.DarkGray; }
+        }
+    }
+}
